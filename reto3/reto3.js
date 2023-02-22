5b1be82fbce2199a49cee19469bba356f1eafb59
~DavidGomez <davidg220588@gmail.com>
2023-02-22 19:28:54 -0300

    reto3

---
+++ reto3/reto3.js
@@ -0,0 +1,14 @@
+/*let addButton = document.getElementById('add').addEventListener('click', function(){
+    alert("hola")
+})
+*/
+let nombreTarea = document.getElementById('nombreTarea');
+let personal = document.getElementById('personal');
+let domestica = document.getElementById('domestica');
+let entretenimiento = document.getElementById('entretenimiento');
+let laboral = document.getElementById('laboral')
+let descripcion = document.getElementById('textArea')
+let alta = document.getElementById('alta');
+let media = document.getElementById('media')
+let baja = document.getElementById('baja')
+
